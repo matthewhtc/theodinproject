1a229289637958e24d0a4390b78dd7c24b6fe6b6
Matthew Chan <Matthew@Matthew-Chans-MacBook-Pro.local>
2016-07-13 14:50:47 -0700

    update etch-a-sketch

--- etch-a-sketch/scripts/sketch.js
+++ etch-a-sketch/scripts/sketch.js
@@ -6,58 +6,64 @@ $(document).ready(function() {
 	var $clear = $("#clear");
 	var $trail = $("#trail");
 	var size = 16; //default grid size is 16x16
+	var option = true;
 
 	//making the grid 
-	var createGrid = function(size) {
+	var createGrid = function(size, option) {
 		for(var i = 0; i < size; i++) {
 			for(var j = 0; j < size; j++) {
 				$grid.append("<div class='box'></div>");
 			}
 		$grid.append("<br>");
 		}	
+
+		if (option) {
+			hover();
+		}
+		else {
+			trail();
+		}
 	}
 
 	var hover = function() {
 		$('.box').hover(function() {
  			$(this).css("background-color", "black");
- 	});
-	}
+ 		});
+	};
 
 	//declare toggle between trail function
- 	var toggle = function() {
+ 	var trail = function() {
+ 		
+
  		$('.box').hover(function() {
- 			$(this).css("background-color", "black");
- 		}), function() {
- 			$(this).css("background-color", "#e45151");
- 		}
+ 			$(this).css("opacity", 0);
+ 		}, function() {
+ 			$(this).fadeTo('fast', 1);
+ 			//$(this).css("background-color", "#e45151;");
+ 		});
  	};
 
-	createGrid(size);
-	hover();
+	createGrid(size, option);
 	
+
 	//clear grid
  	$clear.on('click', function() {
  		size = prompt("Please enter the number of squares per side to make the new grid:");
  		//remove grid
  		$grid.empty();
  		//call createGrid function
- 		createGrid(size);
- 		hover();
+ 		createGrid(size, option);
  	});
 
  	//toggle btw trail
  	$trail.on('click', function() {
- 		toggle();
+ 		size = prompt("Please enter the number of squares per side to make the new grid:");
+ 		//remove grid
+ 		$grid.empty();
+ 		option = false;
+ 		//call createGrid function
+ 		createGrid(size, option);
+ 		
  	});
 
- 	//toggle between trail
- 	var toggle = function() {
- 		$('.box').hover(function() {
- 			$(this).css("background-color", "black");
- 		}), function() {
- 			$(this).css("background-color", "#e45151");
- 		}
- 	};
-
- 
 });
