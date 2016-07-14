385a753d31872f69405a0b247f1fc42e49a67f14
Matthew Chan <Matthew@Matthew-Chans-MacBook-Pro.local>
2016-07-13 23:36:41 -0700

    update etch-a-sketch

--- etch-a-sketch/scripts/sketch.js
+++ etch-a-sketch/scripts/sketch.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
 	var $grid = $(".grid");
 	var $clear = $("#clear");
 	var $trail = $("#trail");
-	var size = 16; //default grid size is 16x16
-	var option = true;
+	var $randomColour = $("#random");
+	var $erase = $("#erase");
+	var $default = $("#default");
+	var size = 50; //default grid size is 50x50
+	var option = 1;
+	var confirm = true;
 
 	//making the grid 
 	var createGrid = function(size, option) {
@@ -17,8 +21,8 @@ $(document).ready(function() {
 		$grid.append("<br>");
 		}	
 
-		if (option) {
-			hover();
+		if (option===1 || option===3) {
+			hover(option);
 		}
 		else {
 			trail();
@@ -26,27 +30,41 @@ $(document).ready(function() {
 	}
 
 	var hover = function() {
-		$('.box').hover(function() {
- 			$(this).css("background-color", "black");
- 		});
+		if (option ===1 || option===2) {
+			$('.box').hover(function() {
+ 				$(this).css("background-color", "black");
+ 			});
+		}
+		else {
+			$('.box').hover(function() {
+				var randCol = randomColour();
+ 				$(this).css("background-color", randCol);
+ 			});
+ 		}
 	};
 
 	//declare toggle between trail function
  	var trail = function() {
- 		
-
- 		$('.box').hover(function() {
+		$('.box').hover(function() {
  			$(this).css("opacity", 0);
  		}, function() {
  			$(this).fadeTo('fast', 1);
- 			//$(this).css("background-color", "#e45151;");
  		});
+ 	};
+
+	//random color generator generator
+ 	var randomColour = function() {
+ 		var r = Math.floor((Math.random()*255));
+ 		var g = Math.floor((Math.random()*255)); 
+ 		var b = Math.floor((Math.random()*255)); 
+ 		return 'rgb(' + r + ',' + g + ',' + b + ')';
+
  	};
 
 	createGrid(size, option);
 	
 
-	//clear grid
+	//clear grid button
  	$clear.on('click', function() {
  		size = prompt("Please enter the number of squares per side to make the new grid:");
  		//remove grid
@@ -55,15 +73,35 @@ $(document).ready(function() {
  		createGrid(size, option);
  	});
 
- 	//toggle btw trail
+ 	//reset button
+ 	$erase.on('click', function() {
+ 		$grid.empty();
+ 		createGrid(size, option); 
+ 	})
+
+ 	//default button resests to default settings
+ 	$default.on('click', function() {
+ 		$grid.empty();
+ 		size = 50;
+ 		option = 1;
+		createGrid(size, option);
+ 	})
+
+	//random colour button
+ 	$randomColour.on('click', function() {
+ 		$grid.empty();
+ 		option = 3;
+ 		createGrid(size, option);
+ 	})
+
+ 	//change to trail button
  	$trail.on('click', function() {
  		size = prompt("Please enter the number of squares per side to make the new grid:");
  		//remove grid
  		$grid.empty();
- 		option = false;
+ 		option = 2;
  		//call createGrid function
  		createGrid(size, option);
  		
  	});
-
 });
